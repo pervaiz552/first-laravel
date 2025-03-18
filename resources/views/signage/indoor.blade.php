a6822b8e4746a3424e994ddb69d473e3ce330e98
pervaiz552 <pervaiz@ystsol.com>
2025-03-18 10:15:26 +0500

    add heading and paragraph

--- resources/views/signage/indoor.blade.php
+++ resources/views/signage/indoor.blade.php
@@ -7,6 +7,8 @@
 </head>
 <body>
     <h1>Lorem, ipsum.</h1>
-    <p>Lorem ipsum dolor sit amet.</h1>
+    <p>Lorem ipsum dolor sit amet.</p>
+    <h2>Lorem ipsum dolor sit.</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 </body>
 </html>
